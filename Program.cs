1c5f3366a0d08c042cee56cb19546f77ec1688cc
gusta <gustavoandres_r@hotmail.com>
2023-08-30 19:53:19 +0200

    Primera Parte de expresiones booleanas

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// Uso del operador de igualdad
+
+/*
+Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string value1 = " a";
+string value2 = "A ";
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+*/
+// Trim() para quitar espacios y ToLower() para poner todo en minìsculas.
+
+/*
+Console.WriteLine("a" != "a");
+Console.WriteLine("a" != "A");
+Console.WriteLine(1 != 2);
+
+string myValue = "a";
+Console.WriteLine(myValue != "a");
+*/
+
+/*
+Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1);
+*/
+
+/*
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
+
+// These two lines of code will create the same output
+
+Console.WriteLine(pangram.Contains("fox") == false);
+Console.WriteLine(!pangram.Contains("fox"));
+*/
+
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("cow"));
+
+/*
+El término "negación lógica" se refiere al operador !. Algunas personas llaman a este 
+operador el "operador not". Cuando coloca el operador ! antes de una expresión 
+condicional (o cualquier código que se evalúe como true o false), obliga al código 
+a asegurarse de que la expresión sea falsa.
+*/

33ebbd2eb03ee6266947a3982d7fc756fddd225d
gusta <gustavoandres_r@hotmail.com>
2023-09-06 20:02:45 +0200

    Iteración de un bloque de código mediante la instrucción for parte 2

--- 04_iterationBlockCode.cs
+++ 04_iterationBlockCode.cs
@@ -164,3 +164,32 @@ foreach (var name in names)
 
 // Ejercicio: Realización de una actividad de desafío con instrucciones for e if
 
+/*
+Desafío FizzBuzz
+
+Estas son las reglas de FizzBuzz que necesita implementar en el proyecto de código:
+
+Valores de salida de 1 a 100, un número por línea, dentro del bloque de código de una instrucción de iteración.
+Si el valor actual es divisible por 3, se imprime el término Fizz junto al número.
+Si el valor actual es divisible por 5, se imprime el término Buzz junto al número.
+Si el valor actual es divisible por 3 y por 5, se imprime el término FizzBuzz junto al número.
+*/
+
+for (int i = 1; i < 101; i++)
+{
+    if ((i % 3 == 0) && (i % 5 == 0))
+        Console.WriteLine($"{i} - FizzBuzz");
+    else if (i % 3 == 0)
+        Console.WriteLine($"{i} - Fizz");
+    else if (i % 5 == 0)
+        Console.WriteLine($"{i} - Buzz");
+    else
+        Console.WriteLine($"{i}");
+}
+
+/*
+La instrucción for es importante porque permite recorrer en iteración el bloque de código 100 veces.
+if-elseif-else permite buscar los divisores de 3 y 5.
+%, el operador mod, permite determinar si 3 o 5 se dividen por otro número sin un resto.
+Asimismo, el operador && garantiza que se puede dividir un número en 3 y 5 para la condición FizzBuzz.
+*/
